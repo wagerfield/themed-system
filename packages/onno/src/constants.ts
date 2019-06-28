492eed99db0be34e65d53f2b0232af73be0b38f9
Matthew Wagerfield <matthew@wagerfield.com>
2019-06-28 17:13:35 +0100

    chore: cast breakpoints

--- packages/onno/src/constants.ts
+++ packages/onno/src/constants.ts
@@ -4,6 +4,6 @@ export const PX_SCALE: Lookup = [0, 2, 4, 8, 16, 32, 64, 128, 256, 512]
 
 export const PC_SCALE: Lookup = Object.assign([], PX_SCALE, { 1: "100%" })
 
-export const BREAKPOINTS: Breakpoints = ["all", "sm", "md", "lg", "xl"].map(
+export const BREAKPOINTS = ["all", "xs", "sm", "md", "lg", "xl"].map(
   (alias, index) => ({ alias, value: index * 360 })
-)
+) as Breakpoints
