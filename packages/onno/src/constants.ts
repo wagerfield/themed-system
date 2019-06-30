5cf882b0f2ff6c502cc5c21bc2a44c055bb67983
Matthew Wagerfield <matthew@wagerfield.com>
2019-07-01 00:30:09 +0100

    refactor: remove breakpoint keys

--- packages/onno/src/constants.ts
+++ packages/onno/src/constants.ts
@@ -4,16 +4,11 @@ export const PX_SCALE: Lookup = [0, 2, 4, 8, 16, 32, 64, 128, 256, 512]
 
 export const PC_SCALE: Lookup = Object.assign([], PX_SCALE, { 1: "100%" })
 
-export const BREAKPOINT_KEYS: BreakpointKey[] = [
+export const BREAKPOINTS: Breakpoints = ([
   "all",
   "xs",
   "sm",
   "md",
   "lg",
   "xl"
-]
-
-export const BREAKPOINTS: Breakpoints = BREAKPOINT_KEYS.map((alias, index) => ({
-  alias,
-  value: index * 360
-}))
+] as BreakpointKey[]).map((alias, index) => ({ alias, value: index * 360 }))
