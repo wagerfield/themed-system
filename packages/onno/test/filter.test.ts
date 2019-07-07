fecd768eeef3c331dcf3f32317be489639ed6533
Matthew Wagerfield <matthew@wagerfield.com>
2019-07-07 12:49:11 +0100

    test: filter types

--- packages/onno/test/filter.test.ts
+++ packages/onno/test/filter.test.ts
@@ -1,7 +1,15 @@
+/* tslint:disable:no-unused-expression */
+
 import { filter, omit, pick } from "../src/filter"
 import { AnyFunction } from "./test-utils"
 
-const props = {
+interface Props {
+  a: "a" | 1
+  b: "b" | 2
+  c: "c" | 3
+}
+
+const props: Props = {
   a: "a",
   b: "b",
   c: "c"
@@ -28,6 +36,11 @@ test("omit", () => {
   expect(omit(["a"])(props)).toEqual({ b: "b", c: "c" })
   expect(omit(["b", "c"])(props)).toEqual({ a: "a" })
   expect(omit(["a", "b", "c"])(props)).toEqual({})
+
+  const result = omit(["a"])(props)
+  // result.a // error
+  result.b // (property) b: "b" | 2
+  result.c // (property) c: "c" | 3
 })
 
 test("pick", () => {
@@ -38,4 +51,9 @@ test("pick", () => {
   expect(pick(["a"])(props)).toEqual({ a: "a" })
   expect(pick(["b", "c"])(props)).toEqual({ b: "b", c: "c" })
   expect(pick(["a", "b", "c"])(props)).toEqual({ a: "a", b: "b", c: "c" })
+
+  const result = pick(["a"])(props)
+  result.a // (property) a: "a" | 1
+  // result.b // error
+  // result.c // error
 })
